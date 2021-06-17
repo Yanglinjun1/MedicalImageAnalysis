7f2fecd5d0455438df6c0db92b71dc0333454755
liyang <linjun-yang@uiowa.edu>
2021-06-17 16:45:37 -0500

    finished the Unet2D

---
+++ models/Unet2D.py
@@ -0,0 +1,38 @@
+import torch.nn as nn
+from .utils import Depthwise_Conv2D, ConvBlock2D, Encoder2D, Decoder2D
+
+class Unet2D(nn.Module):
+    def __init__(self, conv_type="regular"):
+        super().__init__()
+        Conv2d = nn.Conv2d if conv_type == "regular" else Depthwise_Conv2D
+
+        self.encoder1 = Encoder2D(3, 64, conv_type)
+        self.encoder2 = Encoder2D(64, 128, conv_type)
+        self.encoder3 = Encoder2D(128, 256, conv_type)
+        self.encoder4 = Encoder2D(256, 512, conv_type)
+
+        self.center_block = ConvBlock2D(512, 1024, conv_type)
+
+        self.decoder4 = Decoder2D(512, conv_type)
+        self.decoder3 = Decoder2D(256, conv_type)
+        self.decoder2 = Decoder2D(128, conv_type)
+        self.decoder1 = Decoder2D(64, conv_type)
+
+        self.output_end = nn.Sequential(Conv2d(64, 1, kernel_size=3,
+                                               stride=1, padding=1),
+                                        nn.BatchNorm2d(num_features=1),
+                                        nn.Sigmoid())
+
+    def forward(self, x):
+        x1, x2 = self.encoder1(x)
+        x2, x3 = self.encoder2(x2)
+        x3, x4 = self.encoder3(x3)
+        x4, x5 = self.encoder4(x4)
+        x5 = self.center_block(x5)
+
+        d4 = self.decoder4(x5, x4)
+        d3 = self.decoder3(d4, x3)
+        d2 = self.decoder2(d3, x2)
+        d1 = self.decoder1(d2, x1)
+
+        return self.output_end(d1)
