462950ea4bede1103619799a89c954e76e5a7f3b
liyang <linjun-yang@uiowa.edu>
2021-06-18 12:31:28 -0500

    tested all the utils modules

--- models/utils.py
+++ models/utils.py
@@ -4,7 +4,7 @@ import torch.nn.functional as F
 
 class Depthwise_Conv2D(nn.Module):
     """
-    This module implements the depth-wise 2D convolution
+    This module implements the depth-wise 2D convolution.
     """
     def __init__(self, in_chn, out_chn,
                  kernel_size=3, stride=1, padding=1):
@@ -15,31 +15,46 @@ class Depthwise_Conv2D(nn.Module):
 
     def forward(self, x):
         x = self.ConvPerChannel(x)
-        x = self.OneByOne(x)
-
-        return x
-
-
-class ConvBlock2D(nn.Module):
-    def __init__(self, in_chn, out_chn, conv_type = "regular"):
-        super().__init__()
-        Conv2d = nn.Conv2d if conv_type == "regular" else Depthwise_Conv2D
-
-        self.conv1 = Conv2d(in_chn, out_chn, kernel_size=3, stride=1, padding=1)
-        self.bn1 = nn.BatchNorm2d(num_features=out_chn)
-        self.conv2 = Conv2d(out_chn, out_chn, kernel_size=3, stride=1, padding=1)
-        self.bn2 = nn.BatchNorm2d(num_features=out_chn)
-
-    def forward(self, x):
-        x = F.leaky_relu((self.bn1(self.conv1(x))))
-        out = F.leaky_relu((self.bn2(self.conv2(x))))
+        out = self.OneByOne(x)
 
         return out
 
+class CBLayer2D(nn.Module):
+    """
+    This module implements convolution & batch-normalization & leaky_relu activation function.
+    """
+    def __init__(self, in_chn, out_chn, kernel_size=3, stride=1, padding=1, conv_type="regular"):
+        super().__init__()
+        Conv2d = nn.Conv2d if conv_type == "regular" else Depthwise_Conv2D
+
+        self.conv = Conv2d(in_chn, out_chn, kernel_size, stride, padding)
+        self.bn = nn.BatchNorm2d(num_features=out_chn)
+
+    def forward(self, x):
+        out = F.leaky_relu(self.bn(self.conv(x)))
+
+        return out
+
+class ConvBlock2D(nn.Module):
+    """
+    This module implements the two-convolution block used in U-net's encoder and decoder.
+    """
+    def __init__(self, in_chn, out_chn, kernel_size=3, stride=1, padding=1, conv_type="regular"):
+        super().__init__()
+
+        self.CBL1 = CBLayer2D(in_chn, out_chn, kernel_size, stride, padding, conv_type)
+        self.CBL2 = CBLayer2D(out_chn, out_chn, kernel_size, stride, padding, conv_type)
+
+    def forward(self, x):
+        return self.CBL2(self.CBL1(x))
+
 class Encoder2D(nn.Module):
+    """
+    This module implements the encoder part of U-net, which output the result of convblock2D and pooling.
+    """
     def __init__(self, in_chn, out_chn, conv_type="regular"):
         super().__init__()
-        self.convblock = ConvBlock2D(in_chn, out_chn, conv_type)
+        self.convblock = ConvBlock2D(in_chn, out_chn, conv_type=conv_type)
 
     def forward(self, x):
         x1 = self.convblock(x)
@@ -48,13 +63,17 @@ class Encoder2D(nn.Module):
         return x1, x2
 
 class Decoder2D(nn.Module):
+    """
+    This module implements the decoder part of U-net: start with the upsampling module
+    and followed by a convblock2D.
+    """
     def __init__(self, out_chn, conv_type="regular"):
         super().__init__()
         Conv2d = nn.Conv2d if conv_type == "regular" else Depthwise_Conv2D
         self.up = nn.Sequential(nn.Upsample(scale_factor=2),
                                 Conv2d(2*out_chn, out_chn, kernel_size=3, stride=1, padding=1),
                                 nn.BatchNorm2d(num_features=out_chn))
-        self.convblock = ConvBlock2D(2*out_chn, out_chn, conv_type)
+        self.convblock = ConvBlock2D(2*out_chn, out_chn, conv_type=conv_type)
 
     def forward(self, x1, x2):
         x1 = F.leaky_relu(self.up(x1))
